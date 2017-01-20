14600a50c7d9574cbf5ef18dda333fd7e3ec1937
Jordan Suchow <suchow@post.harvard.edu>
2017-01-20 02:00:43 -0500

    Improve formatting

--- demos/memoryexpt/static/scripts/experiment.js
+++ demos/memoryexpt/static/scripts/experiment.js
@@ -2,212 +2,215 @@ var uniqueWords = [];
 var currentNodeId;
 
 $(document).ready(function() {
+  // Print the consent form.
+  $("#print-consent").click(function() {
+    window.print();
+  });
 
-    // Print the consent form.
-    $("#print-consent").click(function() {
-        window.print();
-    });
+  // Consent to the experiment.
+  $("#consent").click(function() {
+    store.set("hit_id", getUrlParameter("hit_id"));
+    store.set("worker_id", getUrlParameter("worker_id"));
+    store.set("assignment_id", getUrlParameter("assignment_id"));
+    store.set("mode", getUrlParameter("mode"));
 
-    // Consent to the experiment.
-    $("#consent").click(function() {
-        store.set("hit_id", getUrlParameter("hit_id"));
-        store.set("worker_id", getUrlParameter("worker_id"));
-        store.set("assignment_id", getUrlParameter("assignment_id"));
-        store.set("mode", getUrlParameter("mode"));
+    allow_exit();
+    window.location.href = "/instructions/instructions-1";
+  });
 
-        allow_exit();
-        window.location.href = '/instructions/instructions-1';
-    });
+  // Do not consent to the experiment.
+  $("#no-consent").click(function() {
+    allow_exit();
+    self.close();
+  });
 
-    // Do not consent to the experiment.
-    $("#no-consent").click(function() {
-        allow_exit();
-        self.close();
-    });
+  // Proceed to the waiting room.
+  $("#go-to-waiting-room").click(function() {
+    allow_exit();
+    window.location.href = "/waiting";
+  });
 
-    // Proceed to the waiting room.
-    $("#go-to-waiting-room").click(function() {
-        allow_exit();
-        window.location.href = '/waiting';
-    });
+  // Send a message.
+  $("#send-message").click(function() {
+    send_message();
+  });
 
-    // Send a message.
-    $("#send-message").click(function() {
-        send_message();
-    });
+  // Leave the chatroom.
+  $("#leave-chat").click(function() {
+    leave_chatroom();
+  });
 
-    // Leave the chatroom.
-    $("#leave-chat").click(function() {
-        leave_chatroom();
-    });
-
-    // Submit the questionnaire.
-    $("#submit-questionnaire").click(function() {
-        submitResponses();
-        submitAssignment();
-    });
+  // Submit the questionnaire.
+  $("#submit-questionnaire").click(function() {
+    submitResponses();
+    submitAssignment();
+  });
 });
 
 // Create the agent.
-create_agent = function () {
-    reqwest({
-        url: "/node/" + participant_id,
-        method: "post",
-        type: "json",
-        success: function (resp) {
-            currentNodeId = resp.node.id;
-            getWordList();
-        },
-        error: function (err) {
-            console.log(err);
-            errorResponse = JSON.parse(err.response);
-            if (errorResponse.hasOwnProperty("html")) {
-                $("body").html(errorResponse.html);
-            } else {
-                allow_exit();
-                go_to_page("questionnaire");
-            }
-        }
-    });
-};
-
-getWordList = function () {
-
-    reqwest({
-        url: "/node/" + currentNodeId + "/received_infos",
-        method: "get",
-        type: "json",
-        success: function (resp) {
-            var wordList = JSON.parse(resp.infos[0].contents);
-            showWordList(wordList);
-        },
-        error: function (err) {
-            console.log(err);
-            errorResponse = JSON.parse(err.response);
-            $('body').html(errorResponse.html);
-        }
-    });
-};
-
-showWordList = function (wl) {
-    if (wl.length === 0){ // Show filler task.
-        showFillerTask();
-
-    } else { // Show the next word.
-        $("#wordlist").html(wl.pop());
-        setTimeout(function () {
-            showWordList(wl);
-        }, 2000);
+create_agent = function() {
+  reqwest({
+    url: "/node/" + participant_id,
+    method: "post",
+    type: "json",
+    success: function(resp) {
+      currentNodeId = resp.node.id;
+      getWordList();
+    },
+    error: function(err) {
+      console.log(err);
+      errorResponse = JSON.parse(err.response);
+      if (errorResponse.hasOwnProperty("html")) {
+        $("body").html(errorResponse.html);
+      } else {
+        allow_exit();
+        go_to_page("questionnaire");
+      }
     }
+  });
 };
 
-showFillerTask = function () {
-
-    $("#stimulus").hide();
-    $("#fillertask-form").show();
-
-    setTimeout(function(){
-        showExperiment();
-    }, 30000);
+getWordList = function() {
+  reqwest({
+    url: "/node/" + currentNodeId + "/received_infos",
+    method: "get",
+    type: "json",
+    success: function(resp) {
+      var wordList = JSON.parse(resp.infos[0].contents);
+      showWordList(wordList);
+    },
+    error: function(err) {
+      console.log(err);
+      errorResponse = JSON.parse(err.response);
+      $("body").html(errorResponse.html);
+    }
+  });
 };
 
-showExperiment = function () {
-    $("#fillertask-form").hide();
-    submitResponses();
-    $("#response-form").show();
-    $("#send-message").removeClass("disabled");
-    $("#send-message").html("Send");
-    $("#reproduction").focus();
-    get_transmissions();
+showWordList = function(wl) {
+  if (wl.length === 0) {
+    // Show filler task.
+    showFillerTask();
+  } else {
+    // Show the next word.
+    $("#wordlist").html(wl.pop());
+    setTimeout(
+      function() {
+        showWordList(wl);
+      },
+      2000
+    );
+  }
 };
 
-get_transmissions = function () {
-    reqwest({
-        url: "/node/" + currentNodeId + "/transmissions",
-        method: "get",
-        type: "json",
-        data: {
-            status: "pending",
+showFillerTask = function() {
+  $("#stimulus").hide();
+  $("#fillertask-form").show();
+
+  setTimeout(
+    function() {
+      showExperiment();
+    },
+    30000
+  );
+};
+
+showExperiment = function() {
+  $("#fillertask-form").hide();
+  submitResponses();
+  $("#response-form").show();
+  $("#send-message").removeClass("disabled");
+  $("#send-message").html("Send");
+  $("#reproduction").focus();
+  get_transmissions();
+};
+
+get_transmissions = function() {
+  reqwest({
+    url: "/node/" + currentNodeId + "/transmissions",
+    method: "get",
+    type: "json",
+    data: { status: "pending" },
+    success: function(resp) {
+      transmissions = resp.transmissions;
+      for (var i = transmissions.length - 1; i >= 0; i--) {
+        displayInfo(transmissions[i].info_id);
+      }
+    },
+    complete: function(err) {
+      setTimeout(
+        function() {
+          get_transmissions(nodeId);
         },
-        success: function (resp) {
-          transmissions = resp.transmissions;
-          for (var i = transmissions.length - 1; i >= 0; i--) {
-            displayInfo(transmissions[i].info_id);
-          }
-        },
-        complete: function (err) {
-            setTimeout(function(){
-                get_transmissions(nodeId);
-            }, 1000);
-        }
-    });
+        1000
+      );
+    }
+  });
 };
 
 displayInfo = function(infoId) {
-    reqwest({
-        url: "/info/" + currentNodeId + "/" + infoId,
-        method: "get",
-        type: "json",
-        success: function (resp) {
-            var word = resp.info.contents.toLowerCase();
-            // if word hasn't appeared before, load into unique array and display
-            if (uniqueWords.indexOf(word) === -1){
-              uniqueWords.push(word);
-              $("#reply").append("<p>" + word + "</p>");
-            }
-        },
-    });
+  reqwest({
+    url: "/info/" + currentNodeId + "/" + infoId,
+    method: "get",
+    type: "json",
+    success: function(resp) {
+      var word = resp.info.contents.toLowerCase();
+      // if word hasn't appeared before, load into unique array and display
+      if (uniqueWords.indexOf(word) === -1) {
+        uniqueWords.push(word);
+        $("#reply").append("<p>" + word + "</p>");
+      }
+    }
+  });
 };
 
-send_message = function () {
+send_message = function() {
+  response = $("#reproduction").val();
+  // typing box
+  // don't let people submit an empty response
+  if (response.length === 0) {
+    return;
+  }
 
-      response = $("#reproduction").val(); // typing box
+  // let people submit only if word doesn't have a space
+  if (response.indexOf(" ") >= 0) {
+    $("#send-message").removeClass("disabled");
+    $("#send-message").html("Send");
+    return;
+  }
 
-      // don't let people submit an empty response
-      if (response.length === 0){
-        return;
-      }
+  // will not let you add a word that is non-unique
+  if (uniqueWords.indexOf(response.toLowerCase()) === -1) {
+    uniqueWords.push(response.toLowerCase());
+    $(
+      "#reply"
+    ).append("<p style='color: #1693A5;'>" + response.toLowerCase() + "</p>");
+  } else {
+    $("#send-message").removeClass("disabled");
+    $("#send-message").html("Send");
+    return;
+  }
 
-      // let people submit only if word doesn't have a space
-      if (response.indexOf(' ') >= 0) {
-        $("#send-message").removeClass("disabled");
-        $("#send-message").html("Send");
-        return;
-      }
+  $("#reproduction").val("");
+  $("#reproduction").focus();
 
-      // will not let you add a word that is non-unique
-      if (uniqueWords.indexOf(response.toLowerCase())=== -1){
-        uniqueWords.push(response.toLowerCase());
-        $("#reply").append("<p style='color: #1693A5;'>" + response.toLowerCase() + "</p>");
-      } else {
-        $("#send-message").removeClass("disabled");
-        $("#send-message").html("Send");
-        return;
-      }
-
-    $("#reproduction").val("");
-    $("#reproduction").focus();
-
-    reqwest({
-        url: "/info/" + currentNodeId,
-        method: "post",
-        data: {
-            contents: response,
-            info_type: "Info",
-        },
-        success: function (resp) {
-            $("#send-message").removeClass("disabled");
-            $("#send-message").html("Send");
-        }
-    });
+  reqwest({
+    url: "/info/" + currentNodeId,
+    method: "post",
+    data: { contents: response, info_type: "Info" },
+    success: function(resp) {
+      $("#send-message").removeClass("disabled");
+      $("#send-message").html("Send");
+    }
+  });
 };
 
 leave_chatroom = function() {
-    allow_exit();
-    go_to_page("questionnaire");
+  allow_exit();
+  go_to_page("questionnaire");
 };
 
-$(document).keypress(function (e) {
+$(document).keypress(function(e) {
   if (e.which == 13) {
     $("#send-message").click();
     return false;
@@ -215,82 +218,82 @@ $(document).keypress(function (e) {
 });
 
 // Send participants to the end if there are any infos.
-createParticipant = function () {
+createParticipant = function() {
+  reqwest({
+    url: "/info",
+    method: "get",
+    success: function(resp) {
+      if (resp.info.count > 0) {
+        allow_exit();
+        go_to_page("questionnaire");
+      } else {
+        create_participant();
+      }
+    },
+    error: function(resp) {
+      setTimeout(
+        function() {
+          createParticipant();
+        },
+        1000
+      );
+    }
+  });
+};
+
+quorum = 1000000;
+getQuorum = function() {
+  reqwest({
+    url: "/experiment/quorum",
+    method: "get",
+    success: function(resp) {
+      quorum = resp.quorum;
+    }
+  });
+};
+
+waitForQuorum = function() {
+  // If we haven't gotten the quorum yet, get it.
+  if (quorum >= 1000000 - 1) {
+    getQuorum();
+    // If we haven't created a participant yet, hold tight.
+  } else if (participant_id === undefined || participant_id === "undefined") {}
+  // Otherwise, see if we have enough participants to proceed.
+  else {
     reqwest({
-      url: "/info",
+      url: "/summary",
       method: "get",
-      success: function (resp) {
-        if (resp.info.count > 0) {
-            allow_exit();
-            go_to_page("questionnaire");
-        } else {
-            create_participant();
+      success: function(resp) {
+        summary = resp.summary;
+        n = numReady(resp.summary);
+        percent = Math.round(n / quorum * 100) + "%";
+        $("#waiting-progress-bar").css("width", percent);
+        $("#progress-percentage").text(percent);
+
+        if (n >= quorum) {
+          allow_exit();
+          go_to_page("exp");
         }
-      },
-      error: function (resp) {
-          setTimeout(function(){
-              createParticipant();
-          }, 1000);
       }
     });
+  }
+  setTimeout(
+    function() {
+      waitForQuorum();
+    },
+    1000
+  );
 };
 
-quorum = 1e6;
-getQuorum = function () {
-    reqwest({
-        url: "/experiment/quorum",
-        method: "get",
-        success: function (resp) {
-            quorum = resp.quorum;
-        }
-    });
-};
-
-waitForQuorum = function () {
-
-    // If we haven't gotten the quorum yet, get it.
-    if (quorum >= (1e6-1)) {
-        getQuorum();
-
-    // If we haven't created a participant yet, hold tight.
-    } else if (participant_id === undefined || participant_id === "undefined"){
-        // Do nothing.
+numReady = function(summary) {
+  for (var i = 0; i < summary.length; i++) {
+    if (summary[i][0] == "working") {
+      return summary[i][1];
     }
-
-    // Otherwise, see if we have enough participants to proceed.
-    else {
-        reqwest({
-            url: "/summary",
-            method: "get",
-            success: function (resp) {
-                summary = resp.summary;
-                n = numReady(resp.summary);
-                percent = Math.round((n/quorum)*100.0) + "%";
-                $("#waiting-progress-bar").css("width", percent);
-                $("#progress-percentage").text(percent);
-
-                if (n >= quorum) {
-                    allow_exit();
-                    go_to_page("exp");
-                }
-            },
-        });
-    }
-
-    setTimeout(function () {
-        waitForQuorum();
-    }, 1000);
-};
-
-numReady = function (summary) {
-    for (var i = 0; i < summary.length; i++) {
-        if (summary[i][0] == "working") {
-            return summary[i][1];
-        }
-    }
+  }
 };
 
 // hack for Dallinger 2.0
-submitResponses = function () {
-      submitNextResponse(0);
- };
+submitResponses = function() {
+  submitNextResponse(0);
+};
